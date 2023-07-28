68398156e9a03f0abe0032a89ba27c2611adeed8
Wei <wlin.li2403@gmail.com>
2023-07-28 16:07:40 -0400

    added nav bar and started home banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+// importing bootstrap
+import { Button, Navbar } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+// import components
+import MyNavBar from './component/NavBar'
+import HomepageBanner from './component/HomescreenBanner'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <MyNavBar></MyNavBar>
+
+      <br>
+      </br>
+      <HomepageBanner className="banner"></HomepageBanner>
+
+    </div>
   )
 }
 
