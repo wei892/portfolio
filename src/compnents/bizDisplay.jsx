a3212f6027d4ccfb511d6f62635e35b1410b9302
Wei <wlin.li2403@gmail.com>
2023-10-26 21:02:42 -0400

    added details to my persona as a student and biz owner

---
+++ src/compnents/bizDisplay.jsx
@@ -0,0 +1,27 @@
+
+const BusinessDisplaay = () => {
+    return (
+        <>
+            <div>
+                Entrepenuership?? 
+            </div>
+
+            <div>
+                In my freetime, I am the proud owner of an art account @<a href="">memoli.store</a>, accumulating over 10 thousand followers. I design stickers and paper goods, featuring my origional characters Coco, Creme, and Eggie to share my joy of journaling. I document my behind the scenes and growth on youtube.
+            </div>
+
+            <div> 
+                Check out my shop below!
+                <div>
+                    <div> Instagram </div>
+                    <div> Etsy </div>
+                    <div> Youtube </div>
+                </div>
+            </div>
+        
+        </>
+    )
+
+}
+
+export default BusinessDisplaay
---
+++ src/compnents/studentDisplay.jsx
@@ -0,0 +1,28 @@
+
+const StudentDisplay = () => {
+    return (
+        <>
+            <div> Always Learning </div>
+
+            <div> 
+                What I Am Learning Now:
+                <div>
+
+                </div>
+            </div>
+            
+            <div>
+                What I've Learned 
+                <div>
+                    Blog Site
+                </div>
+                <div>
+                    My Projects
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default StudentDisplay
