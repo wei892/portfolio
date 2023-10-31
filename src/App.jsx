1c860f0d4cbe85fe2eeb3e47030e609c37245641
Wei <wlin.li2403@gmail.com>
2023-10-31 12:23:43 -0400

    removed nav componenet and incorporated it to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-import { Router, useRouteError } from 'react-router-dom'
+import { Router} from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 
 import './cssFiles/app.css'
+import './cssFiles/NavBar.css'
 
 // import components
-import MyNavBar from './pages/NavBar'
+// import MyNavBar from './pages/NavBar'
 import HomepageBanner from './pages/HomescreenBanner'
 import AboutMe from './pages/AboutMe'
 import Exp from './pages/Experiences'
@@ -14,16 +16,41 @@ import Footer from './pages/Footer'
 
 
 function App() {
+
+  let navElement = useRoutes([
+    {path: '/', element: <HomepageBanner/>},
+  ])
+
   return (
     <div>
-      <MyNavBar/>
-      <br></br>
-      <HomepageBanner className="banner" id= 'home'/>
+      {/* <MyNavBar/> */}
+      <header>
+        <div className='navBar navigation'>
+          <div className='leftCont'>
+            <h1> Mimi</h1>
+          </div>
+          <div className='rightCont'>
+            <div className='navButton'> 
+              <h5> Home </h5> 
+            </div>
 
-      <AboutMe> </AboutMe>
-      <Exp />
-      {/* <Projects></Projects> */}
-      {/* <Contacts></Contacts> */}
+            <div className='navButton'> 
+              <h5> Experiences </h5> 
+            </div>
+
+            <div className='navButton'> 
+              <h5> Projects </h5> 
+            </div>
+
+            <div className='navButton'> 
+              <h5> Resume </h5> 
+            </div> 
+          </div>
+        </div>
+      </header>
+
+      {navElement}
+      
 
       <Footer/> 
 
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,31 +1,23 @@
 // components
 import '../cssFiles/NavBar.css'
 
+// 
+import { useRoutes } from 'react-router-dom'
+
 const MyNavBar = () => {
+
+    
+
     return(
-        <div className='navBar navigation'>
-            <div className='leftCont'>
-                <h1> Mimi</h1>
-            </div>
-            <div className='rightCont'>
-                <div className='navButton'> 
-                    <h5> Home </h5> 
-                </div>
+        <>
+            
+            
+            {navElement}
+        </>
 
-                <div className='navButton'> 
-                    <h5> Experiences </h5> 
-                </div>
-
-                <div className='navButton'> 
-                    <h5> Projects </h5> 
-                </div>
-
-                <div className='navButton'> 
-                    <h5> Resume </h5> 
-                </div> 
-            </div>
-        </div>
     )
+
+    
     
 }
 
