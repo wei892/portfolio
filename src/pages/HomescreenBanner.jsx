ed046091a7a9be6826447c0bf70331c755922046
Wei <wlin.li2403@gmail.com>
2023-10-31 12:54:42 -0400

    added links to homepage

--- src/pages/HomescreenBanner.jsx
+++ src/pages/HomescreenBanner.jsx
@@ -1,4 +1,8 @@
+// stylying for the homepage banner
 import '../cssFiles/homesceen.css'
+
+// importing packages
+import { Link } from 'react-router-dom'
 
 const HomepageBanner = () => {
 
@@ -10,22 +14,21 @@ const HomepageBanner = () => {
                         Hi! I'm Mimi Li
                     </h2>
                     <p>
-                        Computer Science student at CUNY - Hunter College with a minor in Mathematics
+                        <b>Computer Science </b> student at <b>CUNY - Hunter College</b> with a minor in Mathematics
                     </p>
                     <div className="linkContainer">
-                        <div> About Me </div>
-                        <div> Linked In </div>
-                        <div> Github </div>
-                    
+                        <div> 
+                            <Link to='../aboutMe'> About Me </Link>
+                        </div>
+                        <div> <a target='_blank'href='https://www.linkedin.com/in/mimi-li03/'> Linked In </a> </div>
+                        <div> <a target='_blank' href='https://github.com/wei892'> Github </a> </div>
                     </div>
                 </div>
                 <div className="bioCont heartCont">  
                     <div className="heart">
                     </div>
                 </div>
-
             </div>
-
         </>
     )
 }
