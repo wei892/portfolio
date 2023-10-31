0b2490e2fd86bfacb66ea131773bcbe9ce6a7c09
Wei <wlin.li2403@gmail.com>
2023-10-31 13:05:25 -0400

    working on about me nagivations

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,15 +1,12 @@
 import '../cssFiles/aboutme.css'
 import portrait from '../imgs/protrait.png'
-import { Route, useRouteError } from 'react-router-dom'
+import { Route, useRoutes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 //import components
 import BusinessDisplaay from '../compnents/bizDisplay'
 import StudentDisplay from '../compnents/studentDisplay'
-
-// let persona = useRoute({
-
-// })
 
 
 const AboutMe = () => {
@@ -17,12 +14,14 @@ const AboutMe = () => {
         <>
         <div className='aboutMeBanner'>
             <br/>
-            {/* <div className='imageCont'> 
-                <img className='aboutMeImage' src={portrait} alt='aboutMeImage'></img>
-            </div> */}
+            <div className='imageCont'> 
+                <img className='aboutMeImage' src={portrait} alt='An image of me'></img>
+            </div>
+
+            <br></br>
 
             <div className='details'>
-                Chech out what I'm doing in each role
+                I am a:
                 <div className='buttonCont'>
                     <div className='button'>
                         Student
@@ -31,13 +30,10 @@ const AboutMe = () => {
                     <div className='button'>
                         Entrepreneuer
                     </div>
-                    
                 </div>
             </div>
 
-            <StudentDisplay />
-            <BusinessDisplaay />
-            
+            {/* {personaElements}             */}
 
         </div>
         
