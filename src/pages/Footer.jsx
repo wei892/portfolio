6af838245e047188546c8f40fb7d7f2ce15898d5
Wei <wlin.li2403@gmail.com>
2023-11-21 16:58:48 -0500

    updated links

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
                 <div > Mimi (Wei Lin)  Li @ 2023 </div> <br></br>
                 <div> Connect with Me: </div> <br></br>
                 <div className='socialsCont'> 
-                    <a className='social' href=''> Github </a>
-                    <a className='social' href=''> Linked In </a>
-                    <a className='social' href=''> Email </a>
+                    <a className='social' target='_blank' href='https://github.com/wei892' > Github </a>
+                    <a className='social' target='_blank'href='https://www.linkedin.com/in/mimi-li03/' > Linked In </a>
+                    <a className='social' href='mailto: wlin.li2403@gmail.com'> Email </a>
                     <a className='social' href=''> Resume </a>
                 </div>
 
